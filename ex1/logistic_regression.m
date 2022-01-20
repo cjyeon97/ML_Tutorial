817aee5171541f5f34331afc32b5329a593c55cb
ziyxxn <38822012+cjyeon97@users.noreply.github.com>
2022-01-20 10:30:08 +0900

    Add files via upload

---
+++ ex1/logistic_regression.m
@@ -0,0 +1,39 @@
+function [f,g] = logistic_regression(theta, X,y)
+  %
+  % Arguments:
+  %   theta - A column vector containing the parameter values to optimize.
+  %   X - The examples stored in a matrix.  
+  %       X(i,j) is the i'th coordinate of the j'th example.
+  %   y - The label for each example.  y(j) is the j'th example's label.
+  %
+
+  m=size(X,2);
+  
+  % initialize objective value and gradient.
+  f = 0;
+  g = zeros(size(theta));
+
+
+  %
+  % TODO:  Compute the objective function by looping over the dataset and summing
+  %        up the objective values for each example.  Store the result in 'f'.
+  %
+  % TODO:  Compute the gradient of the objective by looping over the dataset and summing
+  %        up the gradients (df/dtheta) for each example. Store the result in 'g'.
+  %
+%%% YOUR CODE HERE %%%
+%for j = 1:m
+%  hx = sigmoid(theta'*X(:,j));
+%  f = f + y(j) * log(hx) + (1 - y(j)) * log(1 - hx);
+%  g = g + X(:,j) * (h - y(j));
+%endfor
+for i=1:m
+    % update f
+    h = 1 / (1 + exp(- theta' * X(:,i)));
+    f = f + y(i) * log(h) + (1 - y(i)) * log(1 - h);
+    
+    % update g
+    g = g + X(:,i) * (h - y(i));
+end
+f = -f;
+  
